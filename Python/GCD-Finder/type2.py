beeaf22a7cf9389411d51a7a550e899838f18024
Srijan Bhattacharyya <78896721+SrijanBhattacharyya@users.noreply.github.com>
2023-07-29 19:50:56 +0530

    Add Python/GCD-Finder/type2.py

---
+++ Python/GCD-Finder/type2.py
@@ -0,0 +1,28 @@
+# WAP in python to find the GCD of n numbers.
+def find_gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def find_gcd_of_n_numbers(numbers):
+    result = numbers[0]
+    for num in numbers[1:]:
+        result = find_gcd(result, num)
+    return result
+
+def main():
+    try:
+        n = int(input("Enter the number of elements: "))
+        numbers = []
+        for i in range(n):
+            num = int(input(f"Enter number {i+1}: "))
+            numbers.append(num)
+
+        gcd = find_gcd_of_n_numbers(numbers)
+        print(f"The GCD of {numbers} is {gcd}")
+
+    except ValueError:
+        print("Invalid input. Please enter valid integers.")
+
+if __name__ == "__main__":
+    main()
