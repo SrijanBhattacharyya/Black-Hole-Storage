70746051cf9818e6e2f2929002522d23b6ea4df9
Srijan Bhattacharyya <78896721+SrijanBhattacharyya@users.noreply.github.com>
2023-08-01 01:28:03 +0530

    Update type3.py

--- Python/GCD-Finder/type3.py
+++ Python/GCD-Finder/type3.py
@@ -1,5 +1,5 @@
 # WAP in python to find the GCD of n numbers using PRIME FACTORISATION method.
-def prime_factors(num):
+def fact (num: int) -> list:
     factors = []
     i = 2
     while i * i <= num:
@@ -7,41 +7,40 @@ def prime_factors(num):
             i += 1
         else:
             num //= i
-            factors.append(i)
+            factors.append (i)
     if num > 1:
-        factors.append(num)
+        factors.append (num)
     return factors
 
-def find_gcd_of_n_numbers(numbers):
-    prime_factors_list = [prime_factors(num) for num in numbers]
+def gcd_n (numbers: list) -> int:
+    fact_list = [fact (num) for num in numbers]
 
     common_factors = {}
-    for factors in prime_factors_list:
-        for factor in set(factors):
+    for factors in fact_list:
+        for factor in set (factors):
             if factor in common_factors:
-                common_factors[factor] = min(common_factors[factor], factors.count(factor))
+                common_factors [factor] = min (common_factors[factor], factors.count (factor))
             else:
-                common_factors[factor] = factors.count(factor)
+                common_factors [factor] = factors.count (factor)
 
     gcd = 1
-    for factor, count in common_factors.items():
+    for factor, count in common_factors.items ():
         gcd *= factor ** count
 
     return gcd
 
-def main():
+def main () -> None:
     try:
-        n = int(input("Enter the number of elements: "))
+        n = int (input ("Enter the number of elements: "))
         numbers = []
-        for i in range(n):
-            num = int(input(f"Enter number {i+1}: "))
-            numbers.append(num)
+        for i in range (n):
+            num = int (input ("    Enter a number: "))
+            numbers.append (num)
 
-        gcd = find_gcd_of_n_numbers(numbers)
-        print(f"The GCD of {numbers} is {gcd}")
+        print (f"The GCD of {numbers} is {gcd_n (numbers)}")
 
     except ValueError:
-        print("Invalid input. Please enter valid integers.")
+        print ("Invalid input. Please enter valid integers.")
 
 if __name__ == "__main__":
-    main()
+    main ()
